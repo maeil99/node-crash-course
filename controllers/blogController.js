d013ef4bab6dd05c7d63e7969517df336f4650ef
maeil99 <ismailmohdnorit@gmail.com>
2021-08-05 17:32:30 +0800

    clean up code

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -20,7 +20,7 @@ const blog_details = (req, res) => {
       res.render("blogs/details", { blog: result, tajuk: "Blog details" });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render("404", { tajuk: "Blog not found" });
     });
 };
 
